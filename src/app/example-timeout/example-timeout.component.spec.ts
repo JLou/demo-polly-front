db85a25f9b75e19a2f2fd9b4d24371c350bc3850
JLou <jlou.xd@gmail.com>
2018-05-16 10:43:16 +0200

    Ajout des spinners

--- src/app/example-timeout/example-timeout.component.spec.ts
+++ src/app/example-timeout/example-timeout.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { ExampleTimeoutComponent } from './example-timeout.component';
+
 
 describe('ExampleTimeoutComponent', () => {
   let component: ExampleTimeoutComponent;
